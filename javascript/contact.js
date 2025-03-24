da9bcf336fcff2cf343d2675c9e19e9a95c3bff0
alejandro01espindolaa <alejandro01espindolaa@gmail.com>
2025-03-24 14:58:35 -0600

    done

---
+++ javascript/contact.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Tu mensaje ha sido enviado con éxito!');
+  });
---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.typed', {
+        strings: ['FullStack Java Developer Jr', 'Tech Support Engineer L2', ],
+        typeSpeed: 50,  
+        backSpeed: 30,  
+        backDelay: 2000, 
+        loop: true,  
+        cursorChar: '|',  
+        fadeOut: true,  
+        smartBackspace: true  
+    });
+});
